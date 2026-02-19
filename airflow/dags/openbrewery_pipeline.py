bc0e103f070eefe252d798b2e9de54278395e70c
wellisonS <wsp@cin.ufpe.br>
2026-02-19 10:42:38 -0300

    Adicionando o Dockerfile e a primeira dag.

---
+++ airflow/dags/openbrewery_pipeline.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="openbrewery_pipeline",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["openbrewery"],
+) as dag:
+
+    bronze = DatabricksRunNowOperator(
+        task_id="bronze_ingestion",
+        databricks_conn_id="databricks_default",
+        job_id=1234567890  # <-- vamos trocar isso
+    )
+
+    silver = DatabricksRunNowOperator(
+        task_id="silver_transformation",
+        databricks_conn_id="databricks_default",
+        job_id=0987654321  # <-- vamos trocar isso
+    )
+
+    gold = BashOperator(
+        task_id="dbt_gold",
+        bash_command="""
+        cd /opt/dbt/openbrewery &&
+        dbt run --select gold &&
+        dbt test --select gold
+        """
+    )
+
+    bronze >> silver >> gold
